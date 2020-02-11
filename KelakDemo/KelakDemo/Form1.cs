543542356f9241373fbf5970dadc8f45229e9883
YateCN <1310117981@qq.com>
2020-02-11 22:53:28 +0800

    测试一下这个是什么
    描述2253

---
+++ KelakDemo/KelakDemo/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelakDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //窗体加载事件
+
+        }
+    }
+}
